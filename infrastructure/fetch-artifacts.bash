8539fd3db63246f7d1339c6134a53aefb61e44e9
Jordan Hornblow <jordan@jch254.com>
2017-08-16 14:58:16 +1000

    Fetch artifacts from codepipeline-poc-test in prod pipeline

--- infrastructure/fetch-artifacts.bash
+++ infrastructure/fetch-artifacts.bash
@@ -2,8 +2,6 @@
 
 echo Fetching artifacts via AWS CLI...
 
-ARTIFACT_ARCHIVE_BUCKET=$(cd infrastructure && terraform output artifacts_bucket_id)
-
 aws s3 cp s3://$ARTIFACT_ARCHIVE_BUCKET/$TF_VAR_name/$CODEBUILD_RESOLVED_SOURCE_VERSION.zip $CODEBUILD_RESOLVED_SOURCE_VERSION.zip
 unzip $CODEBUILD_RESOLVED_SOURCE_VERSION.zip
 
