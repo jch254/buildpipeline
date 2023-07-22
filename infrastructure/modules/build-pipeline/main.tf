1280085305047e54449a640f5f136d3f9ab131fe
Jordan Hornblow <jordan@jch254.com>
2023-07-21 20:54:42 -0500

    Fix

--- infrastructure/modules/build-pipeline/main.tf
+++ infrastructure/modules/build-pipeline/main.tf
@@ -71,7 +71,7 @@ resource "aws_codebuild_project" "codebuild_project_in_vpc" {
   count        = var.vpc_id != "" ? 1 : 0
   name         = var.name
   description  = "Builds, tests and deploys ${var.name}"
-  service_role = var.codebuild_role_arn
+  service_role = aws_iam_role.codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
