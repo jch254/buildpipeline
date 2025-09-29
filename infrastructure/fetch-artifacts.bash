451b4eb324ecc7fbfb52d7a4e18ae0af361504c6
Jordan Hornblow <jordan@jch254.com>
2025-09-29 23:00:29 +0700

    Fixer

--- infrastructure/fetch-artifacts.bash
+++ infrastructure/fetch-artifacts.bash
@@ -2,6 +2,8 @@
 
 echo Fetching artifacts via AWS CLI...
 
+ARTIFACT_ARCHIVE_BUCKET=$(cd infrastructure && terraform output -raw artifacts_bucket_id)
+
 aws s3 cp s3://"$ARTIFACT_ARCHIVE_BUCKET/$CODEBUILD_RESOLVED_SOURCE_VERSION".zip "$CODEBUILD_RESOLVED_SOURCE_VERSION".zip
 unzip -q "$CODEBUILD_RESOLVED_SOURCE_VERSION".zip
 
